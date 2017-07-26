6e4dc1979c9a5583fffc348579094b7dbe41fffe
nadrey <nadrey@gmail.com>
2017-07-26 14:37:22 -0400

    Add acc pragmas to color functions

--- mandlebrot_mpi_color.c
+++ mandlebrot_mpi_color.c
@@ -8,27 +8,93 @@ typedef struct complex{
   double imag;
 } Complex;
 
-#pragma acc routine(cal_pixel) seq
-int cal_pixel(Complex c){
-  int count, max_iter;
-  Complex z;
-  double temp, lengthsq;
+typedef unsigned char Color[3];
 
-  max_iter = 255;
-  z.real = 0;
-  z.imag = 0;
-  count = 0;
+const double MAX_LENGTH_SQ = 4;
+const int MAX_ITER = 10000;
+const double MAX_DISTANCE = 1 << 18;
+
+#pragma acc routine(cal_pixel) seq
+void cal_pixel(const Complex &pt, Color &c){
+  int iter;
+  Complex z, dz;
+  double temp, length_sq, distance;
+  z.real = 0; z.imag = 0;
+  dz.real = 0; dz.real = 0;
+  iter = 0;
 
   do{
-    temp = z.real * z.real - z.imag * z.imag + c.real;
-    z.imag = 2 * z.real * z.imag + c.imag;
+    temp = z.real * dz.real - z.imag * dz.imag;
+    dz.imag = 4 * dz.real * z.imag;
+    dz.real = 2 * temp + 1; 
+    temp = z.real * z.real - z.imag * z.imag + pt.real;
+    z.imag = 2 * z.real * z.imag + pt.imag;
     z.real = temp;
-    lengthsq = z.real * z.real + z.imag * z.imag;
-    count ++;
+    length_sq = z.real * z.real + z.imag * z.imag;
+    iter++;
   }
-  while ((lengthsq < 4.0) && (count < max_iter));
-  return(count);
+  while ((lengthsq < MAX_LENGTH_SQ) && (count < MAX_ITER));
+  temp = dz.real * dz.real + dz.imag + dz.imag;
+  distance = 2*std::log(length_sq) * length_sq / temp;
+  cal_color(pixel_size,distance,iter,c);
 }
+
+#pragma acc routine(cal_color) seq
+void cal_color(double pixel_size, double distance, int iter, Color &c) {
+  if(iter >= MAX_ITER) {
+    c[0] = 255; c[1] = 255; c[2] = 255;
+    return;
+  }
+  // compute hsv
+  double temp;
+  temp = std::log(iter) / std::log(MAX_ITER);
+  temp *= 10; temp -= std::floor(temp);
+  double hsv[3], rgb[3];
+  hsv[0] = temp;
+  hsv[1] = 0.7;
+  if(distance < 0.5*pixel_size) {
+    temp = std::pow(2*distance/pixel_size,1./3);
+    hsv[2] = temp;
+  else {
+    hsv[2] = 1;
+  }
+  // convert to hsv
+  hsv_to_rgb(hsv,rgb);
+  // convert to char
+  for(int i = 0; i < 3; ++i) {
+    c[i] = (char)255*rgb[i];
+  }
+}
+
+#pragma acc routine(hsv_to_rgb) seq
+void hsv_to_rgb(const double *hsv, double *rgb) {
+  rgb[0] = 0; rgb[1] = 0; rgb[2] = 0;
+  double c = hsv[1]*hsv[2];
+  double h_prime = hsv[0]/60;
+  double x = c*(1 - std::abs(std::fmod(h_prime,2.0) - 1));
+  if(h_prime >= 0 && h_prime <= 1) {
+    rgb[0] = c; rgb[1] = x;
+  }
+  else if(h_prime >= 1 && h_prime <= 2) {
+    rgb[0] = x; rgb[1] = c;
+  }
+  else if(h_prime >= 2 && h_prime <= 3) {
+    rgb[1] = c; rgb[2] = x;
+  }
+  else if(h_prime >= 3 && h_prime <= 4) {
+    rgb[1] = x; rgb[2] = c;
+  }
+  else if(h_prime >= 4 && h_prime <= 5) {
+    rgb[2] = c; rgb[0] = x;
+  }
+  else if(h_prime >= 5 && h_prime <= 6) {
+    rgb[2] = x; rgb[0] = c;
+  }
+  double m = hsv[2] - c;
+  for(int i = 0; i < 3; i++) {
+    rgb[i] += m;
+  }
+} 
 
 #define MASTERPE 0
 int main(int argc, char **argv){
@@ -36,9 +102,8 @@ int main(int argc, char **argv){
   int i, j;
   Complex c;
   int tmp;
-  double *restrict data_l, *restrict data_l_tmp;
   // for writing out integers
-  char *restrict data_l_char;
+  Color *data;
   int nx, ny;
   int mystrt, myend;
   int nrows_l;
@@ -68,9 +133,8 @@ int main(int argc, char **argv){
   nrows_l = nx/nprocs;
 
   /* create buffer for local work only */
-  data_l = (double *) malloc(nrows_l * ny * sizeof(double));
-  data_l_char = (char *) malloc(nrows_l * ny * sizeof(char));
-#pragma acc enter data create(data_l[0:nrows_l*ny])
+  data = (Color*)malloc(nrows_l * ny * sizeof(Color));
+#pragma acc enter data create(data[0:nrows_l*ny])
   //  data_l_tmp = data_l;
 
   /* calculate each processor's region of work */
@@ -78,25 +142,17 @@ int main(int argc, char **argv){
   myend  = mystrt + nrows_l - 1;
 
   /* calc each procs coordinates and call local mandelbrot set function */
-#pragma acc parallel loop private(c, tmp) collapse(2) present(data_l[0:nrows_l*ny])
+#pragma acc parallel loop private(c, tmp) collapse(2) present(data[0:nrows_l*ny])
   for (i = mystrt; i <= myend; ++i){
     for (j = 0; j < ny; ++j){
       c.real = i/((double) nx) * 4. - 2.;
       c.imag = j/((double) ny) * 4. - 2.;
-      tmp = cal_pixel(c);
-      //      *data_l++ = (double) tmp;
-      data_l[i*ny+j] = (double) tmp;
+      cal_pixel(c);
     }
   }
   //  data_l = data_l_tmp;
-#pragma acc exit data copyout(data_l[0:nrows_l*ny])
+#pragma acc exit data copyout(data[0:nrows_l*ny])
   if (mype == MASTERPE){
-    // convert to integers
-    for (i = mystrt; i <= myend; ++i){
-      for (j = 0; j < ny; ++j){
-        data_l_char[i*ny+j] = (char) data_l[i*ny+j];
-      }
-    }
     file = fopen("mandelbrot.bin_0000", "w");
     printf("nrows_l, ny  %d %d\n", nrows_l, ny);
     fprintf(file,"P5\n");

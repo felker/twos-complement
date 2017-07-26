f3b15a44ffb2091cdd075626029e834f346798d5
nadrey <nadrey@gmail.com>
2017-07-26 15:46:34 -0400

    Fix typo bugs

--- src/mandelbrot_mpi_color.c
+++ src/mandelbrot_mpi_color.c
@@ -49,9 +49,10 @@ void hsv_to_rgb(const double *hsv, double *rgb) {
 #pragma acc routine(cal_color) seq
 void cal_color(double pixel_size, double distance, int iter, Color *c) {
   if(iter >= MAX_ITER) {
-    for(i = 0; i < 2; ++i)
+    for(int i = 0; i < 2; ++i) {
       c[i] = (unsigned char)255;
     }
+    return;
   }
   // compute hsv
   double temp;
